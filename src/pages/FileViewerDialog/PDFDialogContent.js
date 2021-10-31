ecd8320a22325e89af7cb20065ad56e4dbd540bc
Emilio Kartono <e.kartonoe@gmail.com>
2021-10-30 23:44:04 -0700

    feat: added pdf viewer

--- src/pages/FileViewerDialog/PDFDialogContent.js
+++ src/pages/FileViewerDialog/PDFDialogContent.js
@@ -3,10 +3,18 @@ import { useState } from "react";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 
 export default function PDFDialogContent({ fileUrl }) {
+  const [hasLoaded, setHasLoaded] = useState(false);
   const [numPages, setNumPages] = useState(null);
 
   const handlePdfLoadSuccess = ({ numPages }) => {
     setNumPages(numPages);
+    setHasLoaded(true);
+  };
+
+  const renderAllPages = () => {
+    return Array.from(new Array(numPages), (el, index) => (
+      <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+    ));
   };
 
   return (
@@ -16,9 +24,7 @@ export default function PDFDialogContent({ fileUrl }) {
         options={{ workerSrc: "/pdf.worker.js" }}
         onLoadSuccess={handlePdfLoadSuccess}
       >
-        {Array.from(new Array(numPages), (el, index) => (
-          <Page key={`page_${index + 1}`} pageNumber={index + 1} />
-        ))}
+        {hasLoaded && renderAllPages()}
       </Document>
     </>
   );
