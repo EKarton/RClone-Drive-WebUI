2c1fb6c49dfd631d3a5036ce151560238fdfd17a
Emilio Kartono <e.kartonoe@gmail.com>
2021-12-27 00:55:40 -0500

    fix: fix linting

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,7 +3,7 @@
  * Refer to https://github.com/okonet/lint-staged#configuration for more info
  */
 module.exports = {
-  '*.{js,scss,json,md,yaml,yml,html}': ['yarn prettier --write'],
-  '*.js': ['yarn prettier --write'],
-  '*.scss': ['yarn prettier --write'],
+  '*.{json,md,yaml,yml,html}': ['yarn prettier --write'],
+  '*.js': ['eslint --fix', 'yarn prettier --write'],
+  '*.scss': ['stylelint --fix', 'yarn prettier --write'],
 };
