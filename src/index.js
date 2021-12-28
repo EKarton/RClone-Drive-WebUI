5f7b06b4d6314b929dfcb52a8c90d7551bdc37b0
Emilio Kartono <e.kartonoe@gmail.com>
2021-12-28 00:59:14 -0500

    fix: build failure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import 'semantic-ui-css/semantic.min.css';
 import MainApp from 'apps/MainApp';
 import { ColorModeProvider } from 'contexts/ColorMode/index';
 import { RCloneInfoProvider } from 'contexts/RCloneInfo';
